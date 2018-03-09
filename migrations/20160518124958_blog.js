e8b5a0893b23756dd3c37882225c28490ec1666e
Michael Anderson <michanders@msn.com>
2018-03-09 23:40:24 +0000

    automatically backed up by learn

--- migrations/20160518124958_blog.js
+++ migrations/20160518124958_blog.js
@@ -1,8 +1,10 @@
-
 exports.up = function(knex, Promise) {
-  
+  knex.schema.createTable('users').then((tbl) => {
+    tbl.increments();
+    tbl.string('name');
+  }
 };
-
+ 
 exports.down = function(knex, Promise) {
-  
+  knex.schema.destroyTable('users');
 };
